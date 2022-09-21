7c783899cb52b3ed95b6489fca85613d2cd4c87f
Jee Kim <jeehoon.jk75@gmail.com>
2022-09-20 23:46:10 -0700

    Time: 421 ms (11.72%), Space: 15.5 MB (11.31%) - LeetHub

---
+++ 724-find-pivot-index/724-find-pivot-index.py
@@ -0,0 +1,35 @@
+class Solution:
+    def pivotIndex(self, nums: List[int]) -> int:
+        
+        left_prefix = self.prefix_sum(nums)
+        right_prefix = self.reverse(self.prefix_sum(self.reverse(nums))) 
+        
+        print(left_prefix, right_prefix)
+        
+        for i in range(len(nums)):
+            if (left_prefix[i] == right_prefix[i]):
+                return i
+        return -1
+        
+    def prefix_sum(self, nums: List[int]):
+        acc = 0
+        result = []
+        for num in nums:
+            acc += num
+            result.append(acc)
+        return result
+    
+    def reverse(self, nums: List[int]):
+        begin = 0
+        end = len(nums)-1
+        while (begin < end):
+            temp = nums[begin]
+            nums[begin] = nums[end]
+            nums[end] = temp
+            begin = begin + 1
+            end = end - 1
+        return nums
+        
+        
+            
+        
