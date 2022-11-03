cd857a52950f7e65ba3a5fd45bf015518d2e3123
Jee Kim <jeehoon.jk75@gmail.com>
2022-11-02 19:33:42 -0700

    Time: 318 ms (8.26%), Space: 18.7 MB (47.81%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
@@ -1,18 +1,45 @@
+import heapq
+
 class Solution:
+    
     def first_solution(self, nums:List[int], k: int):
+        
+        # Fill in the freq_counter
         freq_counter = {}
         for num in nums:
             freq_counter[num] = freq_counter.setdefault(num, 0) + 1
+
+        # Review this line, (how to sort a dictionary by value)
         freq_counter = dict(sorted(freq_counter.items(), key=lambda item:item[1], reverse=True))
         
+        # Turn the freq_list into a list
         result_list = list(freq_counter.keys())
         
+        # Filter the list
         result = []
         for i in range(k):
             result.append(result_list[i])
         return result
             
-            
+    def second_solution(self, nums:List[int], k:int):
+        
+        # Fill in the freq_counter
+        freq_counter = {}
+        for num in nums:
+            freq_counter[num] = freq_counter.setdefault(num, 0) + 1
+        
+        # Create a heap
+        item_list = [(v*-1, k) for (k, v) in freq_counter.items()]
+        print(item_list)
+        heapq.heapify(item_list)
+        
+        result = []
+        for i in range(k):
+            result.append(heapq.heappop(item_list)[1])
+        return result
+        
+        
+        
             
     
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
@@ -27,7 +54,7 @@ class Solution:
             # That way it can bubble up, or down based on the min-heap property and we can keep taking it
             # I believe the time complexity for a heap was O(log(n))
             
-            return self.first_solution(nums, k)
+            return self.second_solution(nums, k)
             
     
             
