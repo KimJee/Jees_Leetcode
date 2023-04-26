c5ee2c6a5d5b30254ecad418010c5c6eba4e1948
Jee Kim <jeehoon.jk75@gmail.com>
2023-04-26 12:06:11 -0700

    Time: 44 ms (13.43%), Space: 14.2 MB (26.8%) - LeetHub

---
+++ 0022-generate-parentheses/0022-generate-parentheses.py
@@ -0,0 +1,56 @@
+"""
+
+    Input: n = 1
+    Output: ["()"]
+    
+    Input: n = 2
+    Output: (()) ()()
+
+    n = 3
+    Output: ["((()))","(()())","(())()","()(())","()()()"]
+
+    n = 3
+
+             ( 
+        (          )
+     (      )           (
+    )    (           (     )
+  )    )           )           (
+)    )           )                )
+"""
+
+class Solution:
+    def generateParenthesis(self, n: int) -> List[str]:
+        
+        """
+            * We can always start a new [open paren] as long as we don't hit N
+            * We [close paren] as long as we opened a [open paren] before and the count of it is correct
+            * 
+        """
+        stack = []
+        result = []
+        
+        def recursive_parenthesis(openN, closedN) -> str:
+            
+            # If we're out of characters
+            if (openN == closedN == n):
+                result.append("".join(stack))
+
+            if (openN < n):
+                stack.append("(")
+                recursive_parenthesis(openN + 1, closedN)
+                stack.pop()
+            
+            if (closedN < openN):
+                stack.append(")")
+                recursive_parenthesis(openN, closedN + 1)
+                stack.pop()
+        
+        recursive_parenthesis(0, 0)
+        return result
+        
+
+                
+
+                
+                
