cb889785cf23ee64c0a62e7db0f913cb0bc23e78
Jee Kim <jeehoon.jk75@gmail.com>
2023-10-21 13:30:33 -0700

    Time: 1203 ms (32.37%), Space: 0B (100%) - LeetHub

--- 0197-rising-temperature/0197-rising-temperature.sql
+++ 0197-rising-temperature/0197-rising-temperature.sql
@@ -1,4 +1,4 @@
 /* Write your T-SQL query statement below */
-SELECT w1.id 
-  FROM Weather w1, weather w2
-  WHERE w1.temperature > w2.temperature AND DATEDIFF(DAY, w1.recordDate, w2.recordDate) = -1
+SELECT w2.ID
+  FROM Weather w1, Weather w2
+  WHERE w1.temperature < w2.temperature AND DATEDIFF(DAY, w2.recordDate, w1.recordDate) = -1  
